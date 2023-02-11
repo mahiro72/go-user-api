1478fe00639ee928926ba2e763f08d0d0f27f79c
mahiro72 <mahiro.yoshie@gmail.com>
2023-02-11 18:44:44 +0900

    add: graceful shutdown

--- cmd/api/server/main.go
+++ cmd/api/server/main.go
@@ -1,19 +1,58 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/mahiro72/go-user-api/pkg/logger"
 	"github.com/mahiro72/go-user-api/pkg/presenter/http/router"
 	"github.com/mahiro72/go-user-api/pkg/registry"
 )
 
 func main() {
+	repo, err := registry.NewRepository()
+	if err != nil {
+		panic(fmt.Sprintf("error: NewRepository: %v",err))
+	}
 
-	repo, _ := registry.NewRepository()
-	// defer _
 	r := router.New(repo)
 
-	if err := http.ListenAndServe(":8080", r); err != nil {
-		// hoge
+	srv := &http.Server{Addr: "0.0.0.0:3333", Handler: r}
+	srvCtx, srvStopCtx := context.WithCancel(context.Background())
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+
+		// 猶予期間が30秒のグレースフルシャットダウンを開始する
+		shutdownCtx, _ := context.WithTimeout(srvCtx, 30*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				logger.Log("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		// グレースフルシャットダウンのトリガー
+		err := srv.Shutdown(shutdownCtx)
+		if err != nil {
+			logger.Log(err.Error())
+		}
+		srvStopCtx()
+	}()
+
+	err = srv.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		logger.Log(err.Error())
 	}
+
+	// サーバーのcontextが終了するまで待機する
+	<-srvCtx.Done()
 }
